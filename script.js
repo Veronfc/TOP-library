2612e3d63a6150625944dcd3b2bba88a4f8c9b7b
Veronfc <veronfc1701@gmail.com>
2023-09-04 10:47:44 +0200

    add html structure

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const sidebar = document.getElementById('siderbar')
+const main = document.querySelector('.main')
+const openSide = document.getElementById('open-side')
 const lib = []
 
 function book(title, author, pages, read) {
@@ -7,6 +10,41 @@ function book(title, author, pages, read) {
   this.read = read
 }
 
-function addToLib() {
-  
-}
+lib.push(new book('The Hobbit', 'J.R.R. Tolkien', 310, false))
+lib.push(new book('Angels and Demons', 'Dan Brown', 768, true))
+
+lib.forEach((book) => {
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  const title = document.createElement('span');
+  title.className = 'title';
+  title.innerText = book.title;
+
+  const hr = document.createElement('hr');
+
+  const author = document.createElement('span');
+  author.className = 'author';
+  author.innerText = 'by ' + book.author;
+
+  const pages = document.createElement('span');
+  pages.className = 'pages';
+  pages.innerText = book.pages + ' pages';
+
+  const read = document.createElement('button');
+  read.className = 'read';
+  read.innerText = book.read ? 'Read' : 'Not read yet';
+
+  card.appendChild(title)
+  card.appendChild(hr)
+  card.appendChild(author)
+  card.appendChild(pages)
+  card.appendChild(read)
+
+  main.appendChild(card)
+})
+
+openSide.addEventListener('click', () => {
+  sidebar.style.width = '300px'
+
+})
